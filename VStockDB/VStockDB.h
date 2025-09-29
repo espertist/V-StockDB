e8ecb609e36abfb467966edf3ee21c25b743e553
James Hwang <espertist@gmail.com>
2025-09-29 16:02:25 +0900

    first commit

---
+++ VStockDB/VStockDB.h
@@ -0,0 +1,32 @@
+﻿
+// VStockDB.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CVStockDBApp:
+// See VStockDB.cpp for the implementation of this class
+//
+
+class CVStockDBApp : public CWinApp
+{
+public:
+	CVStockDBApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CVStockDBApp theApp;
---
+++ VStockDB/VStockDBDlg.h
@@ -0,0 +1,34 @@
+﻿
+// VStockDBDlg.h : header file
+//
+
+#pragma once
+
+
+// CVStockDBDlg dialog
+class CVStockDBDlg : public CDialogEx
+{
+// Construction
+public:
+	CVStockDBDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_VSTOCKDB_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
